3ecbd4d808629025391f6e05e1a8e2722b01df35
sandip zalte <sandipzalte2002@gmail.com>
2023-07-05 00:19:58 +0530

    Route, API for user credential, and mongoDB cloud setup

---
+++ routes/route.js
@@ -0,0 +1,9 @@
+import express from 'express'
+import { addUser,getUser } from '../controller/user-controller.js';
+const route=express.Router();
+
+
+route.post("/add",addUser);
+route.get("/user",getUser);
+
+export default route; 
